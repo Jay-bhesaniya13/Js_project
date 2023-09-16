389d0d234d0e6f488ce77a9915c4cb47a46669cc
jay bhesaniya <jay>
2023-09-16 15:59:42 +0530

    update 16-9

--- password.js
+++ password.js
@@ -1,12 +1,49 @@
+let c="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+let sc=['!', '@' ,'#' ,'$' ,'%' ,'/' ,'*',"'" ];
+let num=[0,1,2,3,4,5,6,7,8,9]
 
 function fun()
 {
-    let char=document.getElementById('char').checked;
-    let num=document.getElementById('number').checked;
-    if(char)
+    let pass="";
+
+    c_len=document.getElementById('c_len').value;
+    let n_len=document.getElementById('n_len').value;
+    let sc_len=document.getElementById('sc_len').value;
+   
+    if(c_len)
     {
+        for(let i=0;i<c_len;i++)
+        {
+        let y=Math.floor( Math.random()*52 );
+        let tmp=c[y];
+         pass=pass+tmp;
+        
+        }
         
     }
+    if(sc_len)
+    {
+        for(let i=0;i<sc_len;i++)
+        {
+        let y=Math.floor( Math.random()*8 );
+        let tmp=sc[y];
+         pass=pass+tmp;
+        
+        }
+        
+    }
+    if(n_len)
+    {
+        for(let i=0;i<n_len;i++)
+        {
+        let y=Math.floor( Math.random()*10 );
+        let tmp=num[y];
+         pass=pass+tmp;
+        
+        }
+       
+    }
+    console.log(pass);
 
 }
 
--- rps.js
+++ rps.js
@@ -1,97 +1,76 @@
-let comp=  '<p class="com_roll">   <img src="./image/rock.png" > </p> ' ;
-let array=['Rock' , 'Paper' , "Scissor"];
+let comp = '<p class="com_roll">   <img src="./image/rock.png" > </p> ';
+let array = ['Rock', 'Paper', "Scissor"];
 
-let player_point= 0;
+
+let player_point = 0;
 let computer_point = 0;
 
-function player_input(player) {
-    if (player == 'Rock')
-        document.getElementById('player_choose').innerHTML = '<img src="./image/rock.png" class="player_choose">'
-
-    else if (player == 'Paper')
-        document.getElementById('player_choose').innerHTML = '<img src="./image/paper.png" class="player_choose" >'
-
-    else if(player='Scissor') {
-        document.getElementById('player_choose').innerHTML = '<img src="./image/scissors.png" class="player_choose" >'
-
-    }
-
-    computer(player);
-   
+function restart()
+{
+    player_point = 0;
+    computer_point = 0;
 }
 
+function computer() {
+    let x = Math.floor(Math.random() * 3);
 
-function computer(player)
-{
-    let x= Math.floor( Math.random()*3 );
-   
-
-    if( array[x]=='Rock')
-    {
-        document.getElementById('computer_box').innerHTML= '<p class="com_roll">   <img src="./image/rock.png" > </p> '
+    if (array[x] == 'Rock') {
+        document.getElementById('computer_box').innerHTML = '<p class="com_roll">   <img src="./image/rock.png" > </p> '
     }
-    else if( array[x]=='Paper')
-    {
-        document.getElementById('computer_box').innerHTML= '<p class="com_roll">   <img src="./image/paper.png" > </p> '
+    else if (array[x] == 'Paper') {
+        document.getElementById('computer_box').innerHTML = '<p class="com_roll">   <img src="./image/paper.png" > </p> '
     }
-    else{
-        document.getElementById('computer_box').innerHTML= '<p class="com_roll">   <img src="./image/scissors.png" > </p> '
+    else {
+        document.getElementById('computer_box').innerHTML = '<p class="com_roll">   <img src="./image/scissors.png" > </p> '
     }
 
-   round(player,array[x]);
- 
+    return array[x];
 
 }
 
+function total() {
+    document.getElementById('cp').innerHTML = computer_point
+    document.getElementById('pp').innerHTML = player_point
+}
 
-function round(player,comp)
-{
-    player=player.toUpperCase()
-    comp=comp.toUpperCase()
+function round(player) {
+    player = player.toUpperCase()
+    let comp = computer();
+    comp = comp.toUpperCase()
 
-  
-     if(player ==  comp)
-     {
-        document.getElementById('round_display').innerHTML="match drawn ";
-     }
 
-     else if( player=='ROCK' && comp=='SCISSOR')
-     {
-         document.getElementById('round_display').innerHTML="Yahoo!! You win this round";
-         player_point  = player_point + 1;
-     }
-    else if( player=='ROCK' && comp=='PAPER')
-    {
-        document.getElementById('round_display').innerHTML="ohh! You Lost this round";
+    if (player == comp) {
+        document.getElementById('round_display').innerHTML = "match drawn ";
+    }
+
+    else if (player == 'ROCK' && comp == 'SCISSOR') {
+        document.getElementById('round_display').innerHTML = "Yahoo!! You win this round";
+        player_point = player_point + 1;
+    }
+    else if (player == 'ROCK' && comp == 'PAPER') {
+        document.getElementById('round_display').innerHTML = "ohh! You Lost this round";
         computer_point = computer_point + 1;
-        
-    }
-    
 
-    else if( player=='PAPER' && comp=='ROCK')
-    {
-        document.getElementById('round_display').innerHTML="Yahoo!! You win this round ";
-        player_point  = player_point + 1;
     }
-    else if( player=='PAPER' && comp=='SCISSOR')
-    {
-        document.getElementById('round_display').innerHTML=" ohh! You Lost this round";
-        computer_point= computer_point + 1;
+    else if (player == 'PAPER' && comp == 'ROCK') {
+        document.getElementById('round_display').innerHTML = "Yahoo!! You win this round ";
+        player_point = player_point + 1;
+    }
+    else if (player == 'PAPER' && comp == 'SCISSOR') {
+        document.getElementById('round_display').innerHTML = " ohh! You Lost this round";
+        computer_point = computer_point + 1;
+    }
+    else if (player == 'SCISSOR' && comp == 'PAPER') {
+        document.getElementById('round_display').innerHTML = "Yahoo!! You win this round ";
+        player_point = player_point + 1;
+    }
+    else if (player == 'SCISSOR' && comp == 'ROCK') {
+        document.getElementById('round_display').innerHTML = "ohh! You Lost this round";
+        computer_point = computer_point + 1;
     }
 
+    total();
 
-    else if( player=='SCISSOR' && comp=='PAPER')
-    {
-        document.getElementById('round_display').innerHTML="Yahoo!! You win this round ";
-        player_point  = player_point + 1;
-    }
-    else if( player=='SCISSOR' && comp=='ROCK')
-    {
-        document.getElementById('round_display').innerHTML="ohh! You Lost this round";
-        computer_point= computer_point +  1;
-    }
-    
-   
 }
 
 
